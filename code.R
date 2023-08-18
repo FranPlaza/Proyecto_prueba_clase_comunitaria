32ccd6673aa12478e90f5f4b329ce18c3703fa30
Francisco Plaza Vega <f.plaza.vega@gmail.com>
2023-08-18 17:58:37 -0400

    commit nuevo

--- code.R
+++ code.R
@@ -6,7 +6,7 @@ library(credentials)
 create_github_token()
 # Usar token generado en GitHub para vincular proyecto 2 maneras
 gitcreds::gitcreds_set() # vista en clase
-credentials::set_github_pat("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") # usando credentials
+# credentials::set_github_pat("your_PAT") # usando credentials
 
 # Para enlazar con github, después del primer commit
 usethis::use_github()
