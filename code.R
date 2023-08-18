63ba750c8d3da65a113f3de55d7d5fc58007027e
Francisco Plaza Vega <f.plaza.vega@gmail.com>
2023-08-18 16:32:03 -0400

    commit inicial

---
+++ code.R
@@ -0,0 +1,11 @@
+#balcnsaljkfhasdfjkhg
+library(gitcreds)
+library(usethis)
+# Desde GitHub crea token
+create_github_token()
+# Usar token generado en GitHub para vincular proyecto
+gitcreds::gitcreds_set() 
+
+
+# Para enlazar con github, después del primer commit
+usethis::use_github()
